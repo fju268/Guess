dd3752a2653ca8479e71a709cf6b88cbdb820f65
miia <miia@com.tw>
2019-10-22 18:02:13 +0800

    simple

--- app/src/main/java/com/tom/guess/MainActivity.java
+++ app/src/main/java/com/tom/guess/MainActivity.java
@@ -8,27 +8,51 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView num;
+    private TextView count;
+    private ImageView result;
+    int secret = new Random().nextInt(20)+1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        num = findViewById(R.id.number);
+        Log.d("Ding","Secret: "+secret);
         FloatingActionButton fab = findViewById(R.id.fab);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
         });
+    }
+
+    public void guess(View view){
+        int number = Integer.parseInt(num.getText().toString());
+        if(number>secret){
+            Toast.makeText(MainActivity.this,"Smaller",Toast.LENGTH_LONG).show();
+        }else if(number<secret){
+            Toast.makeText(MainActivity.this,"Bigger",Toast.LENGTH_LONG).show();
+        }else {
+            Toast.makeText(MainActivity.this,"Bingo",Toast.LENGTH_LONG).show();
+            result.setImageResource(R.drawable.shit);
+            result.setVisibility(view.VISIBLE);
+        }
+
     }
 
     @Override
